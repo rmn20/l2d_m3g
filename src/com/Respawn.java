009c26acd32a0dd4238dfc750bd7e8cf69c6c601
rmn20 <romax9lahin@gmail.com>
2025-12-27 21:31:48 +0300

    Respawn.createScene deobfuscate

--- src/com/Respawn.java
+++ src/com/Respawn.java
@@ -7,8 +7,9 @@ public class Respawn {
 
 	public Respawn(Vector3D point, House house) {
 		this.point = point;
-		this.part = house.computePart(-1, point.x, point.z);
-		if(this.part == -1) {
+		part = house.computePart(-1, point.x, point.z);
+		
+		if(part == -1) {
 			System.out.println("ERROR: неправильная точка старта " + point.x + " " + point.y + " " + point.z);
 		}
 
@@ -16,56 +17,56 @@ public class Respawn {
 
 	//public u() {}
 	public static Scene createScene(int width, int height, String file) {
-		IniFile ini;
+		IniFile ini = IniFile.createFromResource(file);
 
-		float scale = (ini = IniFile.createFromResource(file)).getFloat("WORLD_SCALE");
+		float scale = ini.getFloat("WORLD_SCALE");
 		Texture tex = Texture.createTexture(ini.getString("WORLD_TEXTURE"));
 		MeshData[] meshes = MeshData.loadMeshes3D2(ini.getString("WORLD_MODEL"), tex.img, scale, false, true);
+		
+		House house = HouseCreator.create(meshes);
+		
 		String skyModel = ini.getString("SKYBOX_MODEL");
-
 		String skyTex = ini.getString("SKYBOX_TEXTURE");
 		Skybox skybox = null;
+		
 		if(skyModel != null && skyTex != null) {
 			skybox = new Skybox(ini.getString("SKYBOX_MODEL"), ini.getString("SKYBOX_TEXTURE"));
+			house.setSkybox(skybox);
 		}
 
-		House var13 = HouseCreator.create(meshes);
-		Respawn var14 = readPoints(ini.getString("START"), var13)[0];
-		Respawn var15 = readPoints(ini.getString("FINISH"), var13)[0];
-		Respawn[] var16 = readPoints(ini.getString("ENEMIES"), var13);
+		Respawn start = readPoints(ini.getString("START"), house)[0];
+		Respawn finish = readPoints(ini.getString("FINISH"), house)[0];
+		Respawn[] spawnPoints = readPoints(ini.getString("ENEMIES"), house);
 
-		int var8 = ini.getInt("ENEMY_COUNT");
-		int var11 = ini.getInt("FREQUENCY");
-		Scene var9;
-		(var9 = new Scene(width, height, var13, var14, var15, var16, var8, var11)).getHouse().setSkybox(skybox);
-		return var9;
+		int enemyCount = ini.getInt("ENEMY_COUNT");
+		int spawnFrequency = ini.getInt("FREQUENCY");
+		
+		Scene scene = new Scene(width, height, house, start, finish, spawnPoints, enemyCount, spawnFrequency);
+		
+		return scene;
 	}
 
 	private static Respawn[] readPoints(String file, House house) {
 		if(file == null) {
 			return null;
 		} else {
-			file = file;
-			StringBuffer var2 = new StringBuffer();
+			StringBuffer strBuf = new StringBuffer();
 
-			int var3;
-			for(var3 = 0; var3 < file.length(); ++var3) {
-				char var4;
-				if((var4 = file.charAt(var3)) != 32) {
-					var2.append(var4);
-				}
+			for(int i = 0; i < file.length(); i++) {
+				char ch = file.charAt(i);
+				if(ch != ' ') strBuf.append(ch);
 			}
 
-			String[] var5;
-			Respawn[] var6 = new Respawn[(var5 = MeshData.cutOnStrings(var2.toString(), ';')).length];
+			String[] strPoints = MeshData.cutOnStrings(strBuf.toString(), ';');
+			Respawn[] spawnPoints = new Respawn[strPoints.length];
 
-			for(var3 = 0; var3 < var6.length; ++var3) {
-				int[] var7 = MeshData.cutOnInts(var5[var3], ',');
-				Vector3D var8 = new Vector3D(var7[0], var7[1], var7[2]);
-				var6[var3] = new Respawn(var8, house);
+			for(int i = 0; i < spawnPoints.length; i++) {
+				int[] xyz = MeshData.cutOnInts(strPoints[i], ',');
+				Vector3D pos = new Vector3D(xyz[0], xyz[1], xyz[2]);
+				spawnPoints[i] = new Respawn(pos, house);
 			}
 
-			return var6;
+			return spawnPoints;
 		}
 	}
 }
